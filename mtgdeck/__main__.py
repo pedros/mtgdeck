f48f3fb6ef2d661bb40751282729d2057f252add
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-01 23:27:16 +0100

    clean up public cli

--- mtgdeck/__main__.py
+++ mtgdeck/__main__.py
@@ -1,7 +1,47 @@
 import sys
-import json
+import argparse
+
 import mtgdeck
 
 
+encodecs = {
+    'decoder': {'auto': mtgdeck.MtgDeckAutoDecoder,
+                'text': mtgdeck.MtgDeckTextDecoder,
+                'mws': mtgdeck.MtgDeckMagicWorkstationDecoder,
+                'cod': mtgdeck.MtgDeckCockatriceDecoder,
+                'octgn': mtgdeck.MtgDeckOCTGNDecoder},
+    'encoder': {'text': mtgdeck.MtgDeckTextEncoder,
+                'mws': mtgdeck.MtgDeckMagicWorkstationEncoder,
+                'cod': mtgdeck.MtgDeckCockatriceEncoder,
+                'octgn': mtgdeck.MtgDeckOCTGNEncoder},
+}
+
+
+def parse_arguments():
+    def action(kind):
+        class ClassAction(argparse.Action):
+            def __call__(self, parser, namespace, value, option_string=None):
+                setattr(namespace, self.dest, encodecs[kind][value])
+        return ClassAction
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-d', '--decoder', help='decoding format',
+                        action=action('decoder'), default='auto',
+                        choices=encodecs['decoder'].keys())
+    parser.add_argument('-e', '--encoder', help='encoding format',
+                        action=action('encoder'), default='text',
+                        choices=encodecs['encoder'].keys())
+    parser.add_argument('-i', '--input', help='input file',
+                        type=argparse.FileType, default=sys.stdin)
+    parser.add_argument('-o', '--output', help='output file',
+                        type=argparse.FileType, default=sys.stdout)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    json.dump(mtgdeck.load(sys.stdin), sys.stdout)
+    args = parse_arguments()
+    mtgdeck.dump(
+        mtgdeck.load(args.input, cls=args.decoder),
+        args.output,
+        cls=args.encoder
+    )
