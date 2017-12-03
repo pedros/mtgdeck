20633521cef485c4eeb9cd1eaf9e36b0112fb7bd
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-03 10:12:40 +0100

    add function docstrings

--- mtgdeck/__init__.py
+++ mtgdeck/__init__.py
@@ -1,3 +1,4 @@
+"""Public API entry-point for mtgdeck"""
 __version__ = '0.0.1'
 __author__ = 'Pedro Silva <psilva+git@pedrosilva.pt>'
 __all__ = [
@@ -31,16 +32,43 @@ from .encoder import (MtgDeckEncodeError,
 
 
 def load(fp, cls=MtgDeckAutoDecoder):
+    """Deserialize ``fp`` (a ``.read()``-supporting file-like object containing an
+    MTG decklist) to a Python object.
+
+    To use a custom ``MTGDeckDecoder`` subclass, specify it with the ``cls``
+    kwarg; otherwise ``MtgDeckAutoDecoder`` is used.
+
+    """
     return cls().load(fp)
 
 
 def loads(string, cls=MtgDeckAutoDecoder):
+    """Deserialize ``string`` (a ``str``, ``bytes`` or ``bytearray`` instance
+    containing an MTG decklist) to a Python object.
+
+    To use a custom ``MTGDeckDecoder`` subclass, specify it with the ``cls``
+    kwarg; otherwise ``MtgDeckAutoDecoder`` is used.
+
+    """
     return cls().loads(string)
 
 
 def dump(obj, fp, cls=MtgDeckTextEncoder):
+    """Serialize ``obj`` as a MTG decklist formatted stream to ``fp`` (a
+    ``.write()``-supporting file-like object).
+
+    To use a custom ``MtgDeckEncoder`` subclass, specify it with the ``cls``
+    kwarg; otherwise ``MtgDeckEncoder`` is used.
+
+    """
     return cls().dump(obj, fp)
 
 
 def dumps(obj, cls=MtgDeckTextEncoder):
+    """Serialize ``obj`` to a MTG decklist formatted ``str``.
+
+    To use a custom ``MtgDeckEncoder`` subclass, specify it with the ``cls``
+    kwarg; otherwise ``MtgDeckEncoder`` is used.
+
+    """
     return cls().dumps(obj)
