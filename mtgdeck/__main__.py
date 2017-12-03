83dabc098ecf9a87def20ddcffec75acddf00e0d
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-03 10:14:00 +0100

    add command line parsing and automatic type coercion

--- mtgdeck/__main__.py
+++ mtgdeck/__main__.py
@@ -1,47 +1,70 @@
+"""mtgdeck - MTG deck list decoder and encoder
+"""
 import sys
 import argparse
 
 import mtgdeck
 
 
-encodecs = {
-    'decoder': {'auto': mtgdeck.MtgDeckAutoDecoder,
-                'text': mtgdeck.MtgDeckTextDecoder,
-                'mws': mtgdeck.MtgDeckMagicWorkstationDecoder,
-                'cod': mtgdeck.MtgDeckCockatriceDecoder,
-                'octgn': mtgdeck.MtgDeckOCTGNDecoder},
-    'encoder': {'text': mtgdeck.MtgDeckTextEncoder,
-                'mws': mtgdeck.MtgDeckMagicWorkstationEncoder,
-                'cod': mtgdeck.MtgDeckCockatriceEncoder,
-                'octgn': mtgdeck.MtgDeckOCTGNEncoder},
-}
+def action(kind):
+    """Return a ClassAction(argparse.Action) for ``kind``."""
+    encodecs = {
+        'decoder': {'default': mtgdeck.MtgDeckAutoDecoder,
+                    'auto': mtgdeck.MtgDeckAutoDecoder,
+                    'text': mtgdeck.MtgDeckTextDecoder,
+                    'mws': mtgdeck.MtgDeckMagicWorkstationDecoder,
+                    'cod': mtgdeck.MtgDeckCockatriceDecoder,
+                    'octgn': mtgdeck.MtgDeckOCTGNDecoder},
+        'encoder': {'default': mtgdeck.MtgDeckTextEncoder,
+                    'text': mtgdeck.MtgDeckTextEncoder,
+                    'mws': mtgdeck.MtgDeckMagicWorkstationEncoder,
+                    'cod': mtgdeck.MtgDeckCockatriceEncoder,
+                    'octgn': mtgdeck.MtgDeckOCTGNEncoder},
+    }
+
+    class ClassAction(argparse.Action):
+        """Map argument string values to a class in module ``kind``."""
+
+        def __call__(self, parser, namespace, value, option_string=None):
+            """Coerce argument value to the appropriate ``kind`` class."""
+            setattr(namespace, self.dest, encodecs[kind][value])
+
+        @staticmethod
+        def choices():
+            """Return all possible ``kind`` strings."""
+            return encodecs[kind].keys()
+
+        @staticmethod
+        def default():
+            """Return default ``kind``."""
+            return encodecs[kind]['default']
+
+    return ClassAction
 
 
 def parse_arguments():
-    def action(kind):
-        class ClassAction(argparse.Action):
-            def __call__(self, parser, namespace, value, option_string=None):
-                setattr(namespace, self.dest, encodecs[kind][value])
-        return ClassAction
-
+    """Parse command line arguments and return a Namespace object."""
+    decoder_action = action('decoder')
+    encoder_action = action('encoder')
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-d', '--decoder', help='decoding format',
-                        action=action('decoder'), default='auto',
-                        choices=encodecs['decoder'].keys())
+                        action=decoder_action,
+                        default=decoder_action.default(),
+                        choices=decoder_action.choices())
     parser.add_argument('-e', '--encoder', help='encoding format',
-                        action=action('encoder'), default='text',
-                        choices=encodecs['encoder'].keys())
+                        action=encoder_action,
+                        default=encoder_action.default(),
+                        choices=encoder_action.choices())
     parser.add_argument('-i', '--input', help='input file',
-                        type=argparse.FileType, default=sys.stdin)
+                        type=argparse.FileType(), default=sys.stdin)
     parser.add_argument('-o', '--output', help='output file',
-                        type=argparse.FileType, default=sys.stdout)
+                        type=argparse.FileType(), default=sys.stdout)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    mtgdeck.dump(
-        mtgdeck.load(args.input, cls=args.decoder),
-        args.output,
-        cls=args.encoder
-    )
+    print(args)
+    mtgdeck.dump(mtgdeck.load(args.input, cls=args.decoder),
+                 args.output,
+                 cls=args.encoder)
