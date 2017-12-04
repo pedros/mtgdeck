a01af42d6c7ed70e7567ef713f24bef897cc3702
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-04 22:58:23 +0100

    clean up ClassAction for argparser

--- mtgdeck/__main__.py
+++ mtgdeck/__main__.py
@@ -1,4 +1,5 @@
-"""mtgdeck - MTG deck list decoder and encoder
+"""mtgdeck - MTG deck list decoder and encoder library and application
+
 """
 import sys
 import argparse
@@ -23,38 +24,30 @@ def action(kind):
     }
 
     class ClassAction(argparse.Action):
-        """Map argument string values to a class in module ``kind``."""
+        """Map argument string values to a class in module ``kind``.
+
+        Set appropriate ``choices`` and ``default`` attributes.
+
+        """
+        def __init__(self, *args, **kwargs):
+            kwargs['choices'] = encodecs[kind].keys()
+            kwargs['default'] = encodecs[kind]['default']
+            super(ClassAction, self).__init__(*args, **kwargs)
 
         def __call__(self, parser, namespace, value, option_string=None):
             """Coerce argument value to the appropriate ``kind`` class."""
             setattr(namespace, self.dest, encodecs[kind][value])
-
-        @staticmethod
-        def choices():
-            """Return all possible ``kind`` strings."""
-            return encodecs[kind].keys()
-
-        @staticmethod
-        def default():
-            """Return default ``kind``."""
-            return encodecs[kind]['default']
 
     return ClassAction
 
 
 def parse_arguments():
     """Parse command line arguments and return a Namespace object."""
-    decoder_action = action('decoder')
-    encoder_action = action('encoder')
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-d', '--decoder', help='decoding format',
-                        action=decoder_action,
-                        default=decoder_action.default(),
-                        choices=decoder_action.choices())
+                        action=action('decoder'))
     parser.add_argument('-e', '--encoder', help='encoding format',
-                        action=encoder_action,
-                        default=encoder_action.default(),
-                        choices=encoder_action.choices())
+                        action=action('encoder'))
     parser.add_argument('-i', '--input', help='input file',
                         type=argparse.FileType(), default=sys.stdin)
     parser.add_argument('-o', '--output', help='output file',
@@ -62,9 +55,12 @@ def parse_arguments():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     args = parse_arguments()
-    print(args)
     mtgdeck.dump(mtgdeck.load(args.input, cls=args.decoder),
                  args.output,
                  cls=args.encoder)
+
+
+if __name__ == '__main__':
+    main()
