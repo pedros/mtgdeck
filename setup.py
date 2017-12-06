e0b85f13ceda7a335d9a6701650c1d764098a34e
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-06 14:31:51 +0100

    set correct version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='mtgdeck',
     packages=['mtgdeck'],
-    version='0.1',
+    version='0.0.1',
     description='MTG deck list decoder and encoder library and application',
     author='Pedro Silva',
     author_email='psilva+git@pedrosilva.pt',
     url='https://github.com/pedros/mtgdeck',
-    download_url='https://github.com/pedros/mtgdeck/archive/0.1.tar.gz',
+    download_url='https://github.com/pedros/mtgdeck/archive/0.0.1.tar.gz',
     keywords=[
         'magic-the-gathering',
         'parser',
