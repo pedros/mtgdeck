496b20de27add9e26e7b42157285e66d9e5e14a1
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-21 22:58:52 +0100

    bump version to 0.0.8

--- src/mtgdeck/__init__.py
+++ src/mtgdeck/__init__.py
@@ -14,7 +14,7 @@ from .encoder import (EncodeError,
                       CockatriceEncoder)
 
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __author__ = 'Pedro Silva <psilva+git@pedrosilva.pt>'
 __all__ = [
     'load', 'loads',
