03b3eab1c2fa06cc983fe9f7f59560398b354693
Pedro Silva <psilva+git@pedrosilva.pt>
2017-12-30 17:48:47 +0100

    point autodoc at src dir

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/mtgdeck'))
 
 
-# -- General configuration ------------------------------------------------
+# -- general configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
